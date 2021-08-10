88f5530dcf2e7aba8b7632498b7aa0b0cdedabcc
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-08-10 23:39:06 +0530

    10/8/21

--- project_1/app_1/views.py
+++ project_1/app_1/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, this is Prashanth")
+    my_dict = {'insert_var' : "This is from application's view.py !"}
+    return render(request , 'app_1/index.html' , context=my_dict)
+
+
 
 
 
