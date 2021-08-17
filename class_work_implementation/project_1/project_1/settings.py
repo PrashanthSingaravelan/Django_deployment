da04f6d49a43c4ecfb7c52ea2770e8812e0337a5
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-08-17 17:16:07 +0530

    17/8/21

--- class_work_implementation/project_1/project_1/settings.py
+++ class_work_implementation/project_1/project_1/settings.py
@@ -72,15 +72,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project_1.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#Database
+#https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
