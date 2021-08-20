fae9f9ab72ce29d2894dcb5f3dc54da30c3eaebc
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-08-20 19:21:36 +0530

    form-validation

--- class_implementation/forms_project_1/app_1/forms.py
+++ class_implementation/forms_project_1/app_1/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 
 class form_name(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    text = forms.CharField(widget = forms.Textarea)
+    name         = forms.CharField()
+    email        = forms.EmailField()
+
+    password         = forms.CharField(widget=forms.PasswordInput)
+    verify_password  = forms.CharField(label="Enter your password again" , widget=forms.PasswordInput)
+
+    text         = forms.CharField(widget = forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super(form_name, self).clean()
+
+        passwd  = all_clean_data['password']
+        vpasswd = all_clean_data['verify_password']
+
+        if (passwd!=vpasswd):
+            raise forms.ValidationError("Both passwords are mis-matching")
+
