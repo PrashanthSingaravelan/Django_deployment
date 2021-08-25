c1d3a1dee3f28abb49660842c3887052f9d68d89
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-08-25 11:24:10 +0530

    template_inheritance

--- class_implementation/template_project_1/app_1/urls.py
+++ class_implementation/template_project_1/app_1/urls.py
@@ -7,8 +7,8 @@ app_name = 'app_1' ## Look the under the app_1 and find the url that matches
     # 1) domain_name/app_1/relative --> gives the relative view
     # 2) domain_name/app_1/other    --> gives the other view
 urlpatterns = [
+    url(r'^other/$',   views.other,   name='other'),
     url(r'^relative/$',views.relative,name='relative'),
-    url(r'^other/$',   views.other,   name='other')
 ]
 
 
--- class_implementation/template_project_1/template_project_1/urls.py
+++ class_implementation/template_project_1/template_project_1/urls.py
@@ -5,5 +5,5 @@ from app_1 import views
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'admin/', admin.site.urls),
-    url(r'^ app_1/' , include('app_1.urls'))
+    url(r'^app_1/' , include('app_1.urls'))
 ]
