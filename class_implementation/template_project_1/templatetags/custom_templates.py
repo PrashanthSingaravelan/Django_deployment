a0bc91ef574e7478d4b4feb4348bb94a2047ecb1
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-08-30 23:54:14 +0530

    30/8/21

--- class_implementation/template_project_1/templatetags/custom_templates.py
+++ class_implementation/template_project_1/templatetags/custom_templates.py
@@ -1,6 +1,7 @@
 from django import template
 
 register = template.Library()
+@register.filter(name='cut')
 
 def cut_func(value,arg):
     # value = "I am in"
@@ -9,7 +10,7 @@ def cut_func(value,arg):
     #      here ''-->(i.e no value), so 'in' will be removed
     return value.replace(arg,'')
 
-register.filter('cut',cut_func) 
+# register.filter('cut',cut_func) 
 
 # 'cut' --> the function when we use the template tag
 # cut_func --> the function that is present in this custom_templates.py
